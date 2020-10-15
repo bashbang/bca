75e0a4477c081e45e7addbfae06b10fa3d129b02
Chris <chris.mullin@nttdata.com>
2020-10-15 15:25:16 -0700

    trivial change

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,6 @@ resource "azurerm_storage_blob" "storage_blob" {
   source                 = "./dist/function.zip"
 }
 
-
 data "azurerm_storage_account_sas" "storage_sas" {
   connection_string = azurerm_storage_account.storage.primary_connection_string
   https_only        = false
