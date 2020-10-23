203e74c222d8ed24537615848fcf9175f241ef54
HypnoMama <sarahhermsen@gmail.com>
2020-10-23 13:18:06 -0700

    my local strapi with content model set up

---
+++ strapi_local/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '017cba56c6737526062a99ef63d72d31'),
+    },
+  },
+});
---
+++ strapi_local/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '648fe376-7187-480c-b825-1e5b77702171'
+};
