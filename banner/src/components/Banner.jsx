6bb3c9c15dc7867cba8ab9137b5aff2b0c46f286
Brendan Beach <Brendan.Beach@nttdata.com>
2020-10-27 11:31:28 -0700

    Added loading images responsively from strapi as well as responsive text

--- banner/src/components/Banner.jsx
+++ banner/src/components/Banner.jsx
@@ -1,33 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './banner2.css';
+import useWindowDimensions from './WindowDimensions'
 
 export default function Banner(props) {
 
+    const {height, width} = useWindowDimensions(); 
+    const determineBackgroundImage = (screenWidth, image) => {
+        if(screenWidth > image.formats.large.width) {
+            return `url(http://localhost:1337${image.url})`;
+        } else  if(screenWidth > image.formats.medium.width) {
+            return `url(http://localhost:1337${image.formats.large.url})`;
+        } else if(screenWidth > image.formats.small.width) {
+            return `url(http://localhost:1337${image.formats.medium.url})`;
+        } else {
+            return `url(http://localhost:1337${image.formats.small.url})`;
+        }
+    };
+
     const addImage = {
-        backgroundImage: `url(http://localhost:1337${props.image})`
+         backgroundImage: determineBackgroundImage(width, props.image)
     }
-    
 
     return (
-       
+
         <div className="main-region">
             <div id="public-home">
                 <div className='section header' >
                     <div className='photo' >
-
-                        <div className="bannerDiv" style={ addImage }>
-                            <div style={{paddingTop: "1.5em"}}>
+                        <div className="bannerDiv" style={addImage}>
+                            <div style={{ paddingTop: "1.5em" }}>
                                 {props.children}
                             </div>
                         </div>
-                    
                     </div>
                 </div>
-                
             </div>
         </div>
 
 
-        
     )
 }
