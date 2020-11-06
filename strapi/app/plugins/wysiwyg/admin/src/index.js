ba68ea9e70d120221b29cff609de59924f3287a2
HypnoMama <sarahhermsen@gmail.com>
2020-11-06 14:24:18 -0800

    add strapi modificaitons - preview button and richtext plugin

---
+++ strapi/app/plugins/wysiwyg/admin/src/index.js
@@ -0,0 +1,34 @@
+import pluginPkg from '../../package.json';
+import Wysiwyg from './components/Wysiwyg';
+import pluginId from './pluginId';
+import App from './containers/App';
+import Initializer from './containers/Initializer';
+import lifecycles from './lifecycles';
+import trads from './translations';
+
+export default strapi => {
+  const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
+
+  const plugin = {
+    blockerComponent: null,
+    blockerComponentProps: {},
+    description: pluginDescription,
+    icon: pluginPkg.strapi.icon,
+    id: pluginId,
+    initializer: () => null,
+    injectedComponents: [],
+    isReady: true,
+    isRequired: pluginPkg.strapi.required || false,
+    leftMenuLinks: [],
+    leftMenuSections: [],
+    mainComponent: null,
+    name: pluginPkg.strapi.name,
+    preventComponentRendering: false,
+    settings: null,
+    trads: {},
+  };
+
+  strapi.registerField({ type: 'wysiwyg', Component: Wysiwyg });
+
+  return strapi.registerPlugin(plugin);
+};
