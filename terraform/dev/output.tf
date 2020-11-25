86ed5b4a6c76cec04bf1954d5bc9f8cd269b8ce7
Chris <chris@bashbang.com>
2020-11-25 10:14:06 -0800

    tf and pipeline updates

--- terraform/dev/output.tf
+++ terraform/dev/output.tf
@@ -14,45 +14,24 @@ output "acr" {
   description = "The name of the Azure Container Registry"
 }
 
-output "cosmos" {
-  value       = module.bca-infrastructure.cosmosdbname
-  description = "The name of the Azure Cosmos Database"
-}
-
-output "cosmosname" {
-  value       = module.bca-infrastructure.cosmosdbname
-  description = "The CostmosDB host name"
-}
-
 output "akv" {
   value       = module.bca-infrastructure.akvname
   description = "The Azure Key Vault Name"
 }
 
-output "cosmosdbprimary_key" {
-  value       = module.bca-infrastructure.cosmosdbprimary_key
-  description = "The cosmosdb host used in a connection string"
+# TODO: remove this once AKV is fully working
+output "psqluid" {
+  value       = module.bca-infrastructure.psqladmin
+  description = "The admin PSQL UID"
 }
 
-# This is now automated done by Terraform.
-# TODO: Is this the best strategy?
-# output "save_db_host_as-AKV-secret" {
-#   value       = "az keyvault secret set --vault-name \"${module.bca-infrastructure.akvname}\" --name \"DBHOST\" --value \"${module.bca-infrastructure.cosmosdbname}.mongo.cosmos.azure.com\""
-#   description = "The command to execute to store the DB Host into AKV"
-# }
+# TODO: remove this once AKV is fully working
+output "psqlpwd" {
+  value       = module.bca-infrastructure.psqlpassword
+  description = "The admin PSQL PWD"
+}
 
-# output "save_db_uid_as-AKV-secret" {
-#   value       = "az keyvault secret set --vault-name \"${module.bca-infrastructure.akvname}\" --name \"DBUID\" --value \"${module.bca-infrastructure.cosmosdbname}"
-#   description = "The CosmosDB user id"
-# }
-
-# output "save_db_pwd_as-AKV-secret" {
-#   value       = "az keyvault secret set --vault-name \"${module.bca-infrastructure.akvname}\" --name \"DBPWD\" --value \"${module.bca-infrastructure.cosmosdbprimary_key}"
-#   description = "The CosmosDB Primray Password"
-# }
-
-# Connection string doesn't appear to be a saved var in Azure so I'm formulating it here ease to devops engineer
-output "mongo_cli_connection_string" {
-  value       = "mongo \"mongodb://${module.bca-infrastructure.cosmosdbname}:${module.bca-infrastructure.cosmosdbprimary_key}@${module.bca-infrastructure.cosmosdbname}.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${module.bca-infrastructure.cosmosdbname}@\""
-  description = "The CLI command to connect to Mongo - includes the mongo executable"
+output "aks-network" {
+  value = what_is_the_AKV_network_for_accessing_psql
+  description = "What is the network range for the aks"
 }
