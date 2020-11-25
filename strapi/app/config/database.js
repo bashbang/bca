d292d58460bdf3a2b9eb0f02aad8bb137f54653b
Chris <chris@bashbang.com>
2020-11-25 14:47:56 -0800

    Updated stapi docker build

--- strapi/app/config/database.js
+++ strapi/app/config/database.js
@@ -1,20 +1,49 @@
+// DB Config for PSQL used on Azure.  Had to pivot from CosmosDB.
+// requires the following environment variables:
+// export DATABASE_HOST="{PQSSERVICENAME}.postgres.database.azure.com"
+// export DATABASE_PORT="5432"
+// export DATABASE_USERNAME="{USERNAME}@{HOSTNAME}"
+// export DATABASE_PASSWORD="{PASSWORD}}"
+// export DATABASE_SSL="true"
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        client: 'mongo',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE'),
-        ssl: env('DATABASE_SSL'),
-      },
+        client: 'postgres',
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''),
+          schema: 'public',
+          ssl: env('DATABASE_SSL', 'true'),
+        },
+      options: {},
     },
   },
 });
+
+// Keeping this here for use with Mongo and docker-compose
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         client: 'mongo',
+//         host: env('DATABASE_HOST', 'localhost'),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         username: env('DATABASE_USERNAME', ''),
+//         password: env('DATABASE_PASSWORD', ''),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE'),
+//         ssl: env('DATABASE_SSL'),
+//       },
+//     },
+//   },
+// });
