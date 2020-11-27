fcc2df160353c18c971de8a71309d8e64130c5af
Chris <chris@bashbang.com>
2020-11-26 18:07:39 -0800

    added csi pipeline and updated some TF code

--- terraform/dev/azure.tf
+++ terraform/dev/azure.tf
@@ -37,8 +37,14 @@
 #   features {}
 # }
 
-
-provider "azurerm" {
-  version = "~>2.0"
-  features {}
-}
+# terraform {
+#   required_providers {
+#     kubernetes = {
+#       source = "hashicorp/kubernetes"
+#     }
+#     azurerm = {
+#       source "hashicorp/azurerm"
+#       version = "~>2.0"
+#     }
+#   }
+# }
